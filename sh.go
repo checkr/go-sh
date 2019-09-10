559a5ff7e2ce5bf1c96b0e504afb5e6490b998e9
zhuojie <zhuojie@checkr.com>
2019-09-10 14:11:31 -0700

    Add color

--- sh.go
+++ sh.go
@@ -37,21 +37,26 @@ import (
 	"time"
 
 	"github.com/codegangsta/inject"
+	"github.com/fatih/color"
 )
 
 type Dir string
 
 type Session struct {
-	inj     inject.Injector
+	Env         map[string]string
+	ShowCMD     bool // enable for debug
+	Stderr      io.Writer
+	Stdin       io.Reader
+	Stdout      io.Writer
+	PromptColor color.Attribute
+	PromptTitle string
+	PromptEnv   map[string]string
+
 	alias   map[string][]string
 	cmds    []*exec.Cmd
 	dir     Dir
+	inj     inject.Injector
 	started bool
-	Env     map[string]string
-	Stdin   io.Reader
-	Stdout  io.Writer
-	Stderr  io.Writer
-	ShowCMD bool // enable for debug
 	timeout time.Duration
 
 	// additional pipe options
@@ -60,9 +65,15 @@ type Session struct {
 }
 
 func (s *Session) writePrompt(args ...interface{}) {
-	var ps1 = fmt.Sprintf("[golang-sh]$")
-	args = append([]interface{}{ps1}, args...)
-	fmt.Fprintln(s.Stderr, args...)
+	envStrs := []string{}
+	if s.PromptEnv != nil {
+		for k, v := range s.PromptEnv {
+			envStrs = append(envStrs, fmt.Sprintf("%s=%s \\\n", k, v))
+		}
+	}
+
+	args = append([]interface{}{s.PromptTitle, strings.Join(envStrs, " ")}, args...)
+	color.New(s.PromptColor).Fprintln(s.Stderr, args...)
 }
 
 func NewSession() *Session {
@@ -71,13 +82,16 @@ func NewSession() *Session {
 		env[key] = os.Getenv(key)
 	}
 	s := &Session{
-		inj:    inject.New(),
-		alias:  make(map[string][]string),
-		dir:    Dir(""),
-		Stdin:  strings.NewReader(""),
-		Stdout: os.Stdout,
-		Stderr: os.Stderr,
-		Env:    env,
+		Env:         env,
+		Stderr:      os.Stderr,
+		Stdin:       strings.NewReader(""),
+		Stdout:      os.Stdout,
+		PromptColor: color.FgWhite,
+		PromptTitle: "[golang-sh]$",
+
+		alias: make(map[string][]string),
+		dir:   Dir(""),
+		inj:   inject.New(),
 	}
 	return s
 }
@@ -136,6 +150,14 @@ func (s *Session) Exec(cmd string, args ...string) error {
 	return s.Call(cmd, args)
 }
 */
+
+func (s *Session) SetPromptEnv(e map[string]string) *Session {
+	s.PromptEnv = e
+	for k, v := range e {
+		s.Env[k] = v
+	}
+	return s
+}
 
 func (s *Session) SetEnv(key, value string) *Session {
 	s.Env[key] = value
